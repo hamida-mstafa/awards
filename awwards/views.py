04cbac214ce61b8768f5848ca221e3e5f0e05798
hamida-mstafa <hamidamstafa@gmail.com>
2019-01-06 11:22:01 +0300

    imported response from django rest framework

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,9 @@
+from __future__ import unicode_literals
+
+from rest_framework.request import Response
+# from rest_framework.views import APIView
+# from rest_framework import status
+# from .serializers import
 from django.shortcuts import render
 
 # Create your views here.
